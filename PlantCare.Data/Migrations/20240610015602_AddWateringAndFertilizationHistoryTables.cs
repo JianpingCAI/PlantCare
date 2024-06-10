5802753426e7a20fcd7a6595c2d1ad9487f34868
Jianping CAI <cai-jianping@outlook.com>
2024-06-10 09:56:39 +0800

    add DB migration

---
+++ PlantCare.Data/Migrations/20240610015602_AddWateringAndFertilizationHistoryTables.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantCare.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWateringAndFertilizationHistoryTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FertilizationHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PlantId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CareTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FertilizationHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FertilizationHistory_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WateringHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PlantId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CareTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WateringHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WateringHistory_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FertilizationHistory_PlantId",
+                table: "FertilizationHistory",
+                column: "PlantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WateringHistory_PlantId",
+                table: "WateringHistory",
+                column: "PlantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FertilizationHistory");
+
+            migrationBuilder.DropTable(
+                name: "WateringHistory");
+        }
+    }
+}
