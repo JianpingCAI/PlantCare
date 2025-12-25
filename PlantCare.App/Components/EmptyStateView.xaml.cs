cbcd37a8886544ed0de89bdc0fa24cc87625e344
Jianping CAI <cai-jianping@outlook.com>
2025-12-25 16:10:26 +0800

    improve button style

---
+++ PlantCare.App/Components/EmptyStateView.xaml.cs
@@ -0,0 +1,67 @@
+using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
+namespace PlantCare.App.Components
+{
+    public partial class EmptyStateView : ContentView
+    {
+        public static readonly BindableProperty IconSourceProperty =
+            BindableProperty.Create(nameof(IconSource), typeof(ImageSource), typeof(EmptyStateView), null);
+
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(EmptyStateView), string.Empty);
+
+        public static readonly BindableProperty MessageProperty =
+            BindableProperty.Create(nameof(Message), typeof(string), typeof(EmptyStateView), string.Empty);
+
+        public static readonly BindableProperty ActionTextProperty =
+            BindableProperty.Create(nameof(ActionText), typeof(string), typeof(EmptyStateView), string.Empty);
+
+        public static readonly BindableProperty ActionCommandProperty =
+            BindableProperty.Create(nameof(ActionCommand), typeof(ICommand), typeof(EmptyStateView), null);
+
+        public static readonly BindableProperty HasActionProperty =
+            BindableProperty.Create(nameof(HasAction), typeof(bool), typeof(EmptyStateView), false);
+
+        public ImageSource IconSource
+        {
+            get => (ImageSource)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        public string ActionText
+        {
+            get => (string)GetValue(ActionTextProperty);
+            set => SetValue(ActionTextProperty, value);
+        }
+
+        public ICommand ActionCommand
+        {
+            get => (ICommand)GetValue(ActionCommandProperty);
+            set => SetValue(ActionCommandProperty, value);
+        }
+
+        public bool HasAction
+        {
+            get => (bool)GetValue(HasActionProperty);
+            set => SetValue(HasActionProperty, value);
+        }
+
+        public EmptyStateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
