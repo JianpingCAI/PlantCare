c9c9b46ad8ed1aa02f1ace03be0e473cfecc5ff7
Jianping CAI <cai-jianping@outlook.com>
2026-01-08 23:13:09 +0800

    change the tab icons

---
+++ PlantCare.App/Resources/Styles/MaterialIconFont.cs
@@ -0,0 +1,91 @@
+namespace PlantCare.App.Resources.Styles
+{
+    /// <summary>
+    /// Material Icons Regular font glyphs
+    /// https://fonts.google.com/icons
+    /// </summary>
+    public static class MaterialIconFont
+    {
+        // Common icons used in the app
+        public const string Home = "\ue88a";
+        public const string HomeOutlined = "\ue8f5";
+        
+        public const string Grass = "\ue391";
+        public const string Plant = "\ue431";
+        public const string Leaf = "\ue06f";
+        public const string PottedPlant = "\ue545";
+
+        public const string WaterDrop = "\ue798";
+        public const string Water = "\ue7e2";
+        public const string Opacity = "\ue667";
+        
+        public const string EventNote = "\ue616";
+        public const string Calendar = "\ue935";
+        public const string CalendarToday = "\ue935";
+        public const string ScheduleOutlined = "\ue942";
+        public const string CheckDone = "\ue862";
+
+        public const string History = "\ue889";
+        public const string HistoryOutlined = "\ue88e";
+        
+        public const string Settings = "\ue8b8";
+        public const string SettingsOutlined = "\ue8bd";
+        public const string MoreVert = "\ue5d4";
+        
+        public const string Add = "\ue145";
+        public const string AddCircle = "\ue147";
+        
+        public const string Delete = "\ue872";
+        public const string Edit = "\ue3c9";
+        public const string EditOutlined = "\ue3e1";
+        
+        public const string Check = "\ue5ca";
+        public const string Close = "\ue5cd";
+        public const string Clear = "\ue16f";
+        
+        public const string Info = "\ue88f";
+        public const string InfoOutlined = "\ue88e";
+        
+        public const string Search = "\ue8b6";
+        public const string SearchOutlined = "\ue8b7";
+        
+        public const string KeyboardArrowUp = "\ue316";
+        public const string KeyboardArrowDown = "\ue318";
+        public const string KeyboardArrowLeft = "\ue314";
+        public const string KeyboardArrowRight = "\ue315";
+        
+        public const string MenuOutlined = "\ue3b9";
+        public const string Menu = "\ue5d2";
+        
+        public const string ArrowBack = "\ue5e0";
+        public const string ArrowForward = "\ue5e1";
+        
+        public const string MoreHoriz = "\ue5d3";
+        
+        public const string Star = "\ue838";
+        public const string StarOutline = "\ue83b";
+        
+        public const string Favorite = "\ue87d";
+        public const string FavoriteBorder = "\ue7e7";
+        
+        public const string Image = "\ue3f4";
+        public const string ImageOutlined = "\ue3f5";
+        
+        public const string NotificationImportant = "\ue7fd";
+        public const string Warning = "\ue002";
+        
+        public const string Done = "\ue876";
+        public const string DoneAll = "\ue877";
+        
+        public const string ThumbUp = "\ue731";
+        public const string ThumbDown = "\ue73a";
+        
+        public const string Share = "\ue80d";
+        public const string ShareOutlined = "\ue80e";
+        
+        public const string IosShare = "\ue6e0";
+        
+        public const string MoreMenu = "\ue5d4";
+        public const string VerticalMenu = "\ue5d4";
+    }
+}
